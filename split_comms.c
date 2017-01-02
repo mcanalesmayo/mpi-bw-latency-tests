06b1d1feee81ab106fdc3cbb28e14f8f7d6bd93d
Marcos Canales <Marcos Canales>
2017-01-02 13:32:55 +0100

    improved local and global reduction performance

--- split_comms.c
+++ split_comms.c
@@ -24,13 +24,13 @@ int main(int argc, char** argv)
     int aux, repetitions;
     long packet_size;
     double start_time, end_time, *res_time;
-    double worst_lat = DBL_MIN, best_lat = DBL_MAX, mean_lat = 0, *lat_vect, bandwith;
+    double local_worst_lat = DBL_MIN, local_best_lat = DBL_MAX, local_mean_lat = 0;
+    double global_worst_lat, global_best_lat, global_mean_lat, bandwith;
     void *dummy;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     MPI_Status status;
     MPI_Group world_group, senders_group;
     MPI_Comm pair_comm, senders_comm;
-    MPI_Datatype lat_vect_type;
     
     // Get params
     if (argc != 4) exit(1);
@@ -71,21 +71,15 @@ int main(int argc, char** argv)
     if (senders_comm != MPI_COMM_NULL){
         MPI_Comm_size(senders_comm, &senders_size);
         MPI_Comm_rank(senders_comm, &senders_rank);
-
-        // Datatype to gather results
-        MPI_Type_contiguous(repetitions, MPI_DOUBLE, &lat_vect_type);
-        MPI_Type_commit(&lat_vect_type);
     
         // Allocate memory on senders, to measure latency
         res_time = malloc(repetitions*sizeof(double));
-    }
 
-    // Root process (it's also world_rank 0)
-    if (senders_rank == 0) {
-        // Allocate memory on root process to gather results
-        lat_vect = malloc(repetitions*senders_size*sizeof(double));
-        // Print info
-        printf("Number of Repetitions %d, Number of Bounces %d, Packet size %d bytes\n", repetitions, n_bursts, packet_size);
+        // Root process
+	    if (senders_rank == 0) {
+	        // Print info
+	        printf("Number of Repetitions %d, Number of Bounces %d, Packet size %d bytes\n", repetitions, n_bursts, packet_size);
+	    }
     }
 
     // Send and receive bursts
@@ -110,38 +104,40 @@ int main(int argc, char** argv)
         }
     }
 
-    // Gather results
-    // Barrier needed to synchronize senders at this point before gathering results
-    // Also need to check MPI_COMM_NULL because some processes are not in the senders communicator group,
-    // which will raise an exception when calling MPI_Barrier or MPI_Gather
+    // Reduce results
+    // Need to check MPI_COMM_NULL because some processes are not in the senders communicator group,
+    // which will raise an exception when calling MPI_Gather
     if (senders_comm != MPI_COMM_NULL){
-        MPI_Barrier(senders_comm);
-        // Root process in senders communicator is also 0
-        MPI_Gather(res_time, 1, lat_vect_type, lat_vect, 1, lat_vect_type, 0, senders_comm);
+    	// Local vector reduction
+    	for(i = 0; i < repetitions; i++){
+    		local_mean_lat += res_time[i];
+    		if (res_time[i] > local_worst_lat) local_worst_lat = res_time[i];
+    		if (res_time[i] < local_best_lat) local_best_lat = res_time[i];
+    	}
+
+        // Global reduction
         // Root process calculates worst, best and mean latency
+        MPI_Reduce(&local_mean_lat, &global_mean_lat, 1, MPI_DOUBLE, MPI_SUM, 0, senders_comm);
+        MPI_Reduce(&local_worst_lat, &global_worst_lat, 1, MPI_DOUBLE, MPI_MAX, 0, senders_comm);
+		MPI_Reduce(&local_best_lat, &global_best_lat, 1, MPI_DOUBLE, MPI_MIN, 0, senders_comm);
+
+        // Root process in senders communicator is 0
         if (senders_rank == 0){
-            for(i = 0; i < repetitions*senders_size; i++){
-                if (lat_vect[i] > worst_lat) worst_lat = lat_vect[i];
-                if (lat_vect[i] < best_lat) best_lat = lat_vect[i];
-                mean_lat += lat_vect[i];
-            }
-            bandwith = (repetitions*n_bursts*packet_size)/mean_lat;
-            mean_lat /= repetitions*senders_size;
+            bandwith = (repetitions*n_bursts*packet_size)/global_mean_lat;
+            global_mean_lat /= repetitions*senders_size;
 
             // Print results
             printf("Bandwith: %.2f MBytes/s\n", bandwith/(1024*1024));
-            printf("Mean latency per packet: %f\n", mean_lat);
-            printf("Worst latency per packet: %f\n", worst_lat);
-            printf("Best latency per packet: %f\n", best_lat);
+            printf("Mean latency per packet: %f\n", global_mean_lat);
+            printf("Worst latency per packet: %f\n", global_worst_lat);
+            printf("Best latency per packet: %f\n", global_best_lat);
         }
     }
 
     // Free allocated memory
     free(dummy);
     if (senders_comm != MPI_COMM_NULL){
-        if (senders_rank == 0) free(lat_vect);
         free(res_time);
-        MPI_Type_free(&lat_vect_type);
         MPI_Comm_free(&senders_comm);
     }
     MPI_Group_free(&senders_group);
